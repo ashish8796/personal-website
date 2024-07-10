54c5dde0f5840c21781db1b02d2cb3c54872245d
Ashish Kumar Saini <ashishsaini.ak@gmail.com>
2024-07-10 17:07:06 +0530

    Fix resume page (#3)
    * Changed the experience in the portfolio and added skills in the resume
    
    * changed the contact page as well
    
    * changed LICENSE and Readme file
    :

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -1,60 +1,101 @@
-import React,{useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import { FaBookReader, FaRegBookmark } from "react-icons/fa";
-import TimelineItem from './TimelineItem';
-import SkillItem from './SkillItem';
+import TimelineItem from "./TimelineItem";
+import Skills from "../../components/Skills";
 
 const experience = [
   {
-    title: "Software Engineer - Mordor Intelligence",
-    date:"Jan, 2022 — Present",
+    title: "Full Stack Developer(APP - React Native) - Gutes Leben",
+    date: "May, 2023 — Present",
     description: [
-                  "Developed RD Pages for the MI official website serving about 50k daily users, utilised EJS for streamlined creation. This pages gives massive lead counts.",
-                  "Added Sharing and Embeding options in MI website, from where user can share to social medias and embed code to some website. We can track share and views count on these.",
-                  "Significantly improved Core Web Vitals (CWV), maintaining a performance rating of above 80%.",
-                  "Developed architecture of CMS-Synapse platform and User Creation part. Here we can create users and manage access for Synapse users.",
-                  "Developed Companies Section and Dashboards on Synapse platform, where user can access Hubs and RDs based on the access type.",
-                  "Developed Lorien frontend which aimed to build Hubs and provide an intuitive way for the analyst to gather and organize the data. Here we can generate and edit RDs and can download whole RDs along with Table of Content(TOC) in PPT and PDF format.",
-                  "Created Market Activities section where we can manage the meta part of RD pages and Created a Robust from from where we can feed media and other data for Hub Landing Page. We can make drafts for these two and Publish them on MI official website as well from Lorien.",
-                  "Worked on Fusion Charts, with it's help we are generating different graphs which are being used across different Platforms in Mordor.",
-                  "Having experience on working all the core projects demonstrating leadership and teamwork abilities."
-                ]
+      "Upgraded React Native to the latest version and implemented performance optimization techniques, utilizing tools such as Flashlight and react-native-fast-image.",
+      "Enhanced app performance by effectively using a combination of memo and useCallback to reduce unnecessary re-renders.",
+      "Implemented CI/CD pipelines for the app using Expo EAS builds, streamlining the deployment process and ensuring consistent delivery.",
+    ],
+    links: [
+      {
+        title: "Android",
+        link: "https://play.google.com/store/apps/details?id=com.gutesleben",
+      },
+      {
+        title: "IOs",
+        link: "https://apps.apple.com/in/app/gutes-leben/id1541165124",
+      },
+    ],
   },
   {
-     title:"Intern Full Stack Engineer - Alt Campus",
-     date:"Jan, 2021 — Dec, 2021",
-     description: [
-                   "Completed an intensive Full Stack Development Internship.",
-                   "Developed high-performance, responsive web applications using React.JS and Express.JS, showcasing expertise in the MERN stack.",
-                   "Deployed projects on platforms like GitHub Pages, Netlify, Vercel, Heroku, and Firebase, ensuring optimal accessibility and functionality, and utilized Git for version control."
-                  ]
-  }
+    title: "Full Stack Developer (CMS - Next Js, React Js) - Gutes Leben",
+    date: "May, 2023 — Present",
+    description: [
+      "Migrated the static React app served using Node.js to Next.js 13, leveraging server-side rendering (SSR) and static site generation (SSG) for better performance.",
+      "Improved the overall performance of the CMS, resulting in faster load times and better SEO capabilities.",
+      "Optimized the architecture of the CMS, enhancing scalability and maintainability.",
+    ],
+    links: [],
+  },
+  {
+    title:
+      "Full Stack Developer (Backend - Node Js, Express Js, MongoDB) - Gutes Leben",
+    date: "May, 2023 — Present",
+    description: [
+      "Developed new APIs to support enhanced functionalities for both the CMS and the mobile app.",
+      "Refactored existing large APIs into smaller, significantly improving performance and maintainability.",
+      "Successfully migrated the entire system from Azure to AWS, utilizing services such as EC2, S3, Lambda, DocumentDB, and more, to enhance scalability and reliability.",
+    ],
+    links: [],
+  },
+  {
+    title: "SDE I - Meesho",
+    date: "Sep, 2021 — Apr, 2023",
+    description: [
+      "Built a Mini-Games App from scratch using Next.js and integrated it into the Mobile App community feature using iframe and webview, enhancing user engagement and functionality.",
+      "Enabled e-commerce on WhatsApp by extending the add-to-cart feature from WhatsApp to Meesho's website, expanding reach to millions of users and driving significant growth in sales.",
+      "Implemented web notifications by integrating Clevertap into the website, improving real-time communication and user experience.",
+      "Analyzed and worked with analytics tools such as Mixpanel, Clevertap, and GA4, contributing to data-driven decision-making and strategic planning.",
+    ],
+    links: [],
+  },
+  {
+    title: "Intern Full Stack Web Developer - Masai School",
+    date: "Jan, 2021 — Sep, 2021",
+    description: [
+      "Completed an intensive Full Stack Development Internship.",
+      "Developed high-performance, responsive web applications using React.JS and Express.JS, showcasing expertise in the MERN stack.",
+      "Deployed projects on platforms like GitHub Pages, Netlify, Vercel, Heroku, and Firebase, ensuring optimal accessibility and functionality, and utilized Git for version control.",
+      "Collaborated in building Masai School platform, contributing to design, development, and enhancement of user-centric features.",
+    ],
+    links: [],
+  },
 ];
 
 const education = [
   {
-    title:"Bachelor Of Technology - IET MJP Rohilkhand University",
-    date:"2016 - 2020",
-    description:["Scored 81.10%", "Internship in Indian Railways", "Worked on Hybrid Braking System for Electric Vehicles"]
+    title: "HLD, LLD, System Design - Scaler Academy",
+    date: "Feb, 2023 - Present",
+    description: [
+      "Studying High-Level Design (HLD) to understand the architecture and components of complex systems.",
+      "Learning Low-Level Design (LLD) to gain detailed knowledge of system components and their interactions.",
+      "Enhancing skills in System Design to create scalable and efficient systems.",
+      "Mastering SQL for effective database management and data manipulation.",
+      "Improving proficiency in Data Structures and Algorithms (DSA) to solve complex problems efficiently.",
+    ],
   },
   {
-    title:"Intermediate - Sacred Hearts Public School",
-    date:"2015 - 2016",
-    description: ["Scored 81.50%"]
+    title: "Bachelor Of Technology - IET MJP Rohilkhand University",
+    date: "2017 - 2020",
+    description: [],
   },
-  {
-    title:"High School - JP Public School",
-    date:"2013 — 2014",
-    description: ["Scored 70.50%"]
-  }
 ];
 
 const Resume = () => {
-    const [skills, setSkills] = useState(null);
-    useEffect(()=> {
-      fetch('data/skills.json').then(res => res.json()).then(data => {
-        setSkills(data)
+  const [skills, setSkills] = useState(null);
+  useEffect(() => {
+    fetch("data/skills.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setSkills(data);
       });
-    }, [])
+  }, []);
 
   return (
     <section>
@@ -69,15 +110,17 @@ const Resume = () => {
           <h3 className="h3">Experience</h3>
         </div>
         <ol className="timeline-list">
-          {
-            experience?.map((item)=>{
-              return <TimelineItem
-                       key={item.title}
-                       title={item.title}
-                       date={item.date}
-                       description={item.description}/>
-            })
-          }
+          {experience?.map((item) => {
+            return (
+              <TimelineItem
+                key={item.title}
+                title={item.title}
+                date={item.date}
+                description={item.description}
+                links={item.links}
+              />
+            );
+          })}
         </ol>
       </div>
 
@@ -90,30 +133,21 @@ const Resume = () => {
           <h3 className="h3">Education</h3>
         </div>
         <ol className="timeline-list">
-          {
-            education?.map((item)=>{
-              return <TimelineItem 
-                        key={item.title}
-                        title={item.title}
-                        date={item.date}
-                        description={item.description}/>
-            })
-          }
+          {education?.map((item) => {
+            return (
+              <TimelineItem
+                key={item.title}
+                title={item.title}
+                date={item.date}
+                description={item.description}
+              />
+            );
+          })}
         </ol>
-
       </div>
 
- 
-
       <div className="skill">
-        <h3 className="h3 skills-title">My skills</h3>
-        <ul className="skills-list content-card">
-          {
-            skills?.map((skill)=>{
-              return <SkillItem key={skill.title} title={skill.title} value={skill.value} />
-            })
-          }
-        </ul>
+        <Skills />
       </div>
     </section>
   );

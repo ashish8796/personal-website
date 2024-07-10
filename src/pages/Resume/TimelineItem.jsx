64c52ad6582f0cab51a2b0bf9b8f22019be26ec7
ashish8796 <ashishsaini.ak@gmail.com>
2024-07-10 16:26:54 +0530

    Changed the experience in the portfolio and added skills in the resume

--- src/pages/Resume/TimelineItem.jsx
+++ src/pages/Resume/TimelineItem.jsx
@@ -1,25 +1,38 @@
-const TimelineItem = ({ title, date, description }) => {
+const TimelineItem = ({ title, date, description, links }) => {
   return (
     <li className="timeline-item">
       <h4 className="h4 timeline-item-title">{title}</h4>
       <span>{date}</span>
-     { typeof(description) === 'string' ?
-      <p className="timeline-text">{description}</p>
-     
-    : <ol>
-      {
-        description.map((item)=>{
-          return     <li key={item} className="timeline-bullet">
-                {item}
-          </li>
 
-        })
-      }
-  
+      {typeof description === "string" ? (
+        <p className="timeline-text">{description}</p>
+      ) : (
+        <ol>
+          {description.map((item) => {
+            return (
+              <>
+                <li key={item} className="timeline-bullet">
+                  {item}
+                </li>
+              </>
+            );
+          })}
+        </ol>
+      )}
+
+      <ol>
+        {links?.map((linkData) => {
+          return (
+            <li key={linkData.title} className="timeline-bullet">
+              <a href={linkData.link} target="_blank" className="w-max">
+                [{linkData.title}]
+              </a>
+            </li>
+          );
+        })}
       </ol>
-     }
     </li>
   );
-}
+};
 
 export default TimelineItem;
